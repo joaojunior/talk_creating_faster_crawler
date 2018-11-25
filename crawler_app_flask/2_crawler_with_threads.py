9191fc6b820697491d4b9f7d13fd5275031004cd
Joao Junior <jcajcefet@yahoo.com.br>
2018-11-25 15:24:43 +0100

    Crawler with threads

---
+++ crawler_app_flask/2_crawler_with_threads.py
@@ -0,0 +1,27 @@
+from threading import Thread
+
+import requests
+
+from constants import URL_FASTER, URL_SLOWLY
+
+
+def crawler(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        print('error')
+    return response.status_code
+
+
+if __name__ == '__main__':
+    threads = []
+    t = [Thread(target=crawler, args=(URL_SLOWLY,))]
+
+    for i in range(20):
+        t = Thread(target=crawler, args=(URL_FASTER,))
+        threads.append(t)
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
