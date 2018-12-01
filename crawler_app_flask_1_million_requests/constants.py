6338cc7c7b43f30c0f00bd30de3836d1f8bbc88f
Joao Junior <jcajcefet@yahoo.com.br>
2018-12-01 20:05:36 +0100

    Fix Variables

--- crawler_app_flask_1_million_requests/constants.py
+++ crawler_app_flask_1_million_requests/constants.py
@@ -1,9 +1,9 @@
 from urllib.parse import urljoin
 
 
-BASE_URL = 'http://127.0.0.1:4000/'
+BASE_URL = 'http://127.0.0.1:4001/'
 
-URL_FASTER = urljoin(BASE_URL, 'text')
+URL_TEXT = urljoin(BASE_URL, 'text')
 
 NUMBER_REQUESTS = 10**6
 BATCH_SIZE = 1000
